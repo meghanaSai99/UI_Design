929b7f83e1a6eb06235a617349741d10adecaa25
meghanaSai99 <meghanasai2801@gmail.com>
2025-03-19 22:33:19 +0530

    Add files via upload

---
+++ Lab4/switchdemo.dart
@@ -0,0 +1,15 @@
+class switchdemo
+{
+	void sdemo(String fruit)
+	{
+		switch(fruit)
+		{
+			case 'Apple'||'apple':
+				print("It is a Fruit");
+			case 'mango'||'Mango':
+				print("It is a Fruit");
+			default:
+				print("Unavailable");
+		}
+	}
+}
