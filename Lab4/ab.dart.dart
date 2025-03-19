2590fcf977075a98c4c50631d6fdcbc1f67a7a31
meghanaSai99 <meghanasai2801@gmail.com>
2025-03-19 22:35:26 +0530

    Delete Lab4/ab.dart.dart

--- Lab4/ab.dart.dart
+++
@@ -1,8 +0,0 @@
-import 'package:lab4/lab4.dart';
-import 'dart:io';
-void main(List<String> arugements){
-	switch_demo sd = switch_demo();
-	print("enter any colour name:");
-	String colour =stdin.readLineSync()!;
-	print(sd.Scase(colour));
-}
